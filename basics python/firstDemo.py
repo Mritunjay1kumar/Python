4eee21e116123f4a4dbc443959096cb4ccf92339
Mritunjay <kumarmritunjay1227@gmail.com>
2024-07-08 20:52:57 +0530

    Added list

---
+++ basics python/firstDemo.py
@@ -0,0 +1,10 @@
+print('Hello')
+a=7;
+print(a)
+str="hello world"
+print(str)
+b,c,d=5,6.4,'Mritunjay'
+print(b,c,d)
+print("{} {}".format(d,c))
+print(type(b))
+print(type(d))
---
+++ basics python/list.py
@@ -0,0 +1,10 @@
+values=[12,34,'Hello',67.89]
+print(values[0])
+print(values[-1])
+print(values[1:-1])
+values.insert(3,"shetty")
+print(values[0:])
+values.append("Mritunjay")
+print(values)
+del values[0]
+print(values)
